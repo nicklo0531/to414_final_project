a2a0dfb2adaa0dd89cf86c923fc66cb3cc6339fa
Lando <lando@M3-185.local>
2025-12-02 18:36:03 -0500

    y

--- Data/split_data.R
+++ Data/split_data.R
@@ -1,4 +1,7 @@
 library(dplyr)
+library(png)
+library(grid)
+library(ggplot2)
 
 shots <- read.csv("data/shots.csv")
 
@@ -29,3 +32,23 @@ X_test  <- X[test_index, , drop = FALSE]
 
 y_train <- factor(y[train_index], levels = c(0, 1))
 y_test  <- factor(y[test_index],  levels = c(0, 1))
+
+# Load data
+shots <- read.csv("data/shots.csv")
+
+# Filter only successful shots (goal == 1)
+shots_made <- shots %>% filter(goal == 1)
+
+rink_img <- readPNG("Images/rink.png")
+g <- rasterGrob(rink_img, width = unit(1,"npc"), height = unit(1,"npc"))
+
+# Plot shot locations on top of rink
+ggplot(shots_made, aes(x = xCord, y = yCord)) +
+  annotation_custom(g, xmin = min(shots_made$xCord), xmax = max(shots_made$xCord),
+                    ymin = min(shots_made$yCord), ymax = max(shots_made$yCord)) +
+  geom_point(color = "red", alpha = 0.6, size = 0.2) +
+  coord_fixed(ratio = 1) +
+  labs(title = "Locations of Made Shots on Hockey Rink",
+       x = "X Coordinate",
+       y = "Y Coordinate") +
+  theme_void()
